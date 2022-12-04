059ac8affb43f83f6905df64b50bea616ed1a6d9
DylanMacres <Dylan.Macres@du.edu>
2022-12-03 21:14:48 -0700

    adding database logic still

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => 
+export const putDb = async (content) =>{ 
 console.log('Push to the Database')
 //creates a connection to the version of the the database we want
 const jateDb = await openDB('jate', 1);
@@ -21,8 +21,34 @@ const jateDb = await openDB('jate', 1);
 //make a transaction to the database
 const tx = jateDb.transaction('jate','readwrite')
 
+//Open the store you want
+const store = tx.objectStore('jate');
+
+//passing the content 
+const request = store.put({id: 1, value: content });
+
+//confirm the request
+const result = await request;
+console.log(result);
+
+};
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.error('getDb not implemented');
 
+//create the connection to the database
+const jateDb = await openDB('jate', 1);
+
+//make the transaction again
+const tx = jateDb.transaction('jate','readonly');
+//open the store we want
+const store = tx.objectStore('jate');
+
+
+
+
+}
 initdb();
